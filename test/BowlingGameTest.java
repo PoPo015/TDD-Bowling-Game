7f0ca20276621bd81995aa6eb6e66a60d863510c
popo015 <kst005109@naver.com>
2021-08-28 21:03:11 +0900

    1. 실패 2. 코드돌아가게 수정 3. 중복제거/ 코드리팩토링

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -10,27 +10,59 @@ class BowlingGameTest {
     private Game game;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         game = new Game();
     }
 
     @Test
     @DisplayName("0개씩 성공")
-    public void canRoll(){
-        for(int i=0; i<20; i++) game.roll(0);
-        assertEquals(0,game.getscore());
+    public void canRoll() {
+        rollMany(20, 0);
+        assertEquals(0, game.getscore());
     }
+
 
     @Test
     @DisplayName("1개씩만 넘김")
-    public void TestAllOnes(){
-        for(int i = 0; i<20; i++){
-            game.roll(1);
-        }
+    public void TestAllOnes() {
+        rollMany(20, 1);
+        assertEquals(20, game.getscore());
+    }
 
-        assertEquals(20,game.getscore());
+    @Test
+    @DisplayName("원스페어")
+    public void testOnSpare(){
+        rollSpare();
+        game.roll(3);
+        rollMany(17,0);
+        assertEquals(16,game.getscore());
+
+    }
+    @Test
+    @DisplayName("원스트라이크")
+    public void testOneStrike(){
+        game.roll(10);  //스트라이크
+        game.roll(3);
+        game.roll(4);
+        rollMany(16,0);
+        assertEquals(24,game.getscore());
+    }
+
+    @Test
+    @DisplayName("300점 만점")
+    public void testPerfectGame(){
+        rollMany(12,10);
+        assertEquals(300, game.getscore());
+    }
 
 
+    private void rollSpare() {
+        game.roll(5);
+        game.roll(5);   //space
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) game.roll(pins);
     }
 
 
